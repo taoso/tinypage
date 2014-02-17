4845db497fa14bd6cfb6208b9a1fb1935f4d56bd
Jalen Lyu <jalen.lyu@gmail.com>
2014-02-17 14:08:58 +0800

    init tinypage

---
+++ README.md
@@ -0,0 +1,19 @@
+TinyPage
+--------
+TinyPage is an extremely light weight template engine.
+
+Install
+=======
+Use composer to install TinyPage.
+
+Add dependence:
+    {
+        "require": {
+            "jlyu/tinypage": "0.0.1"
+        }
+    }
+and run `composer install`. Enjoy it!
+
+Usage
+=====
+coming soon...
---
+++ TinyPage.php
@@ -0,0 +1,31 @@
+<?php
+namespace Jlyu;
+
+class TinyPage {
+    private $basePath = '.';
+    private $ETag = null;
+
+    public function __construct($config=array()) {
+        if (isset($config['base_path'])) {
+            $this->basePath = $config['base_path'];
+            set_include_path(get_include_path() . ':' . $this->basePath);
+        }
+    }
+
+    public function setBasePath($basePath) {
+        $this->basePath = $basePath;
+    }
+
+    public function render($path, $variables) {
+        ob_start();
+        extract($variables);
+        require sprintf("%s/%s", $this->basePath, $path);
+        $output = ob_get_clean();
+        $this->ETag = md5($output);
+        return $output;
+    }
+
+    public function getETag() {
+        return $this->ETag;
+    }
+}
